461be9a6f21ef7f5ff56513f888be34d340a16da
Steven Murawski <steven.murawski@gmail.com>
2015-05-20 19:34:54 -0700

    COM Unzipping requires the target folder to exist Collapsed a few common commands to their aliases to save characters.

--- support/decode_files.ps1
+++ support/decode_files.ps1
@@ -11,14 +11,13 @@ Function Decode-Files($hash) {
   foreach ($key in $hash.keys) {
     $value = $hash[$key]
     $tmp, $tzip, $dst = $Key, $Value["tmpzip"], $Value["dst"]
-    $sMd5 = (Get-Item $tmp).BaseName.Replace("b64-", "")
+    $sMd5 = (gi $tmp).BaseName.Replace("b64-", "")
     $decoded = if ($tzip -ne $null) { $tzip } else { $dst }
     Decode-Base64File $tmp $decoded
-    Remove-Item $tmp -Force
+    rm $tmp -Force
     $dMd5 = Get-MD5Sum $decoded
-    $verifies = $sMd5 -like $dMd5
     if ($tzip) {Unzip-File $tzip $dst;Remove-Item $tzip -Force}
-    New-Object psobject -Property @{ dst = $dst; verifies = $verifies; src_md5 = $sMd5; dst_md5 = $dMd5; tmpfile = $tmp; tmpzip = $tzip }
+    New-Object psobject -Property @{ dst = $dst; verifies = $sMd5 -like $dMd5; src_md5 = $sMd5; dst_md5 = $dMd5; tmpfile = $tmp; tmpzip = $tzip }
   }
 }
 
@@ -34,12 +33,13 @@ Function Invoke-Input($in) {
   $in = $in | rpp
   Decode-Base64File $in ($decoded = "$($in).ps1")
   $expr = Get-Content $decoded | Out-String
-  Remove-Item $in,$decoded -Force
+  rm $in,$decoded -Force
   Invoke-Expression "$expr"
 }
 
 Function Unzip-File($src, $dst) {
   $unpack = $src -replace '\.zip'
+  if (-not (test-path $unpack)) {$null = mkdir $unpack }
   if (Test-IOCompression) {Add-Type -AN System.IO.Compression.FileSystem; [IO.Compression.ZipFile]::ExtractToDirectory($src, $unpack)}
   else {
     Try {$s = New-Object -ComObject Shell.Application; ($dp = $s.NameSpace($unpack)).CopyHere(($z = $s.NameSpace($src)).Items(), 0x610)} Finally {Release-Com $s; Release-Com $z; Release-COM $dp}
